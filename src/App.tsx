3d794044470607d23e6712194aa48a30a0a63b43
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2024-12-13 01:18:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import { AllTasks } from "./components/pages/allTasks/AllTasks";
 import { ActiveTasks } from "./components/pages/activeTasks/ActiveTasks";
 import { DoneTasks } from "./components/pages/doneTasks/DoneTasks";
 import { Navigation } from "./components/navigation/Navigation";
-import { useAppSelector } from "./hooks/hooks";
 import { InputField } from "./components/inputField/InputField";
 import { Route, Routes } from "react-router-dom";
 function App() {
-  const { taskList } = useAppSelector((state) => state.reducer);
-
   return (
     <div className="container">
       <span className="title">TODOS</span>
